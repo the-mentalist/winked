fc2d78ea6bf17cc3234a5932d6c7eec3f7039d39
Navpreet <navpreet.ahuja@hotmail.com>
2017-07-17 21:21:48 +0530

    error handling, default page changed, street view added

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,15 +44,49 @@ function initMap() {
 }
 
 function populateInfoWindow(marker, infowindow) {
+  debugger;
+  marker.setAnimation(google.maps.Animation.BOUNCE);
+  window.setTimeout(function(){
+    marker.setAnimation(null);
+  }, 750);
 	// Check to make sure the infowindow is not already opened on this marker.
 	if (infowindow.marker != marker) {
 	  infowindow.marker = marker;
-	  infowindow.setContent('<div>' + marker.title + '</div>');
-	  infowindow.open(map, marker);
+	  infowindow.setContent('');
+	  
 	  // Make sure the marker property is cleared if the infowindow is closed.
 	  infowindow.addListener('closeclick',function(){
 	    infowindow.setMarker = null;
 	  });
+    var streetViewService = new google.maps.StreetViewService();
+    var radius = 8;
+    // In case the status is OK, which means the pano was found, compute the
+    // position of the streetview image, then calculate the heading, then get a
+    // panorama from that and set the options
+    function getStreetView(data, status) {
+      if (status == google.maps.StreetViewStatus.OK) {
+        var nearStreetViewLocation = data.location.latLng;
+        var heading = google.maps.geometry.spherical.computeHeading(
+          nearStreetViewLocation, marker.position);
+          infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
+          var panoramaOptions = {
+            position: nearStreetViewLocation,
+            pov: {
+              heading: heading,
+              pitch: 20
+            }
+          };
+        var panorama = new google.maps.StreetViewPanorama(
+          document.getElementById('pano'), panoramaOptions);
+      } else {
+        infowindow.setContent('<div>' + marker.title + '</div>' +
+          '<div>No Street View Found</div>');
+      }
+    }
+    // Use streetview service to get the closest streetview image within
+    // 50 meters of the markers position
+    streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+    infowindow.open(map, marker);
 	}
 }
 
@@ -110,4 +144,8 @@ function stopAnimateMarker(rid){
 			marker.setAnimation(null);
 		}
 	});
+}
+
+function googleError(){
+  $('#map').html("Error in Service");
 }
--- assets/js/view.js
+++ assets/js/view.js
@@ -18,20 +18,23 @@ var LocationModel = function(rests, located, locations){
 // location specific collection
 var locations_ko = [
 	new LocationModel(restaurants.slice(0,5), 'Sector 29', {lat: 28.466945, lng: 77.06652}),
- 	new LocationModel(restaurants.slice(5,9), 'Galleria Market', {lat: 28.467252, lng: 77.08202}),
-  new LocationModel(restaurants.slice(9,11), 'Unitech CyberPark', {lat: 28.442781, lng: 77.057076}),
-  new LocationModel(restaurants.slice(11,14), 'DLF Cyber Hub', {lat: 28.495391, lng: 77.088467})
+ 	new LocationModel(restaurants.slice(5,8), 'Galleria Market', {lat: 28.467252, lng: 77.08202}),
+  new LocationModel(restaurants.slice(8,10), 'Unitech CyberPark', {lat: 28.442781, lng: 77.057076}),
+  new LocationModel(restaurants.slice(10,13), 'DLF Cyber Hub', {lat: 28.495391, lng: 77.088467})
 ];
 
 var viewModel = function(){
 	var self = this;
 
+	this.allLocations = ko.observableArray(restaurants);
 	this.availableLocations = ko.observableArray(locations_ko);
 	this.selectedLocation = ko.observable();
 	this.selectedLocation.subscribe(function(newval){
 		if(newval){
+			self.allLocations(null);
 			zoomToArea(newval.locations);
 		}else{
+			self.allLocations(restaurants);
 			zoomToDefault();
 		}
 	});
@@ -39,11 +42,19 @@ var viewModel = function(){
 		debugger;
 	};*/
 	this.infoAvailable = ko.observable({
-		"cuisines": "",
-		"url": "",
-		"average_cost_for_two": ""
+		"cuisines": '',
+		"url": '',
+		"average_cost_for_two": '',
+		"error": ''
 	});
 	this.selectedRestaurant = function(){
+		var innerself = this;
+		// animate
+		animateMarker(this.rid);
+		window.setTimeout(function(){
+			stopAnimateMarker(innerself.rid);
+		}, 750);
+
 		var rid=this.rid;
 		var info = {};
 		//var info = getInfo(this.rid);
@@ -60,16 +71,16 @@ var viewModel = function(){
 				self.infoAvailable(info);
 			},
 			error: function(){
-				//self.infoAvailable("Error in Request");
+				self.infoAvailable({'error': 'Error in Request'});
 			}
 		});
 	};
-	this.animateMarker = function(){
+	/*this.animateMarker = function(){
 		animateMarker(this.rid);
 	};
 	this.stopAnimateMarker = function(){
 		stopAnimateMarker(this.rid);
-	};
+	};*/
 
 };
 
