be7cbfa993d481cf662899ec69e7ec7fb5453b1c
Navpreet <navpreet.ahuja@hotmail.com>
2017-07-18 19:53:41 +0530

    removed street view added static image

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,21 +16,21 @@ function initMap() {
     // mouses over the marker.
     var highlightedIcon = makeMarkerIcon('0091ff');
     for (var i = 0; i < locations.length; i++) {
-    	var position = locations[i].location;
-    	var title = locations[i].title;
-    	var rid = locations[i].rid;
-    	// Create a marker per location, and put into markers array.
-    	var marker = new google.maps.Marker({
-    		position : position,
-    		id : rid,
-    		title : title,
-    		map : map,
-    		icon: defaultIcon
-    	});
-    	markers.push(marker);
-    	// event listner to open the info window
-    	marker.addListener('click', function() {
-    		populateInfoWindow(this, largeInfowindow);
+      var position = locations[i].location;
+      var title = locations[i].title;
+      var rid = locations[i].rid;
+      // Create a marker per location, and put into markers array.
+      var marker = new google.maps.Marker({
+        position : position,
+        id : rid,
+        title : title,
+        map : map,
+        icon: defaultIcon
+      });
+      markers.push(marker);
+      // event listner to open the info window
+      marker.addListener('click', function() {
+        populateInfoWindow(this, largeInfowindow);
       });
       // Two event listeners - one for mouseover, one for mouseout,
       // to change the colors back and forth.
@@ -44,50 +44,34 @@ function initMap() {
 }
 
 function populateInfoWindow(marker, infowindow) {
-  debugger;
   marker.setAnimation(google.maps.Animation.BOUNCE);
   window.setTimeout(function(){
     marker.setAnimation(null);
-  }, 750);
-	// Check to make sure the infowindow is not already opened on this marker.
-	if (infowindow.marker != marker) {
-	  infowindow.marker = marker;
-	  infowindow.setContent('');
-	  
-	  // Make sure the marker property is cleared if the infowindow is closed.
-	  infowindow.addListener('closeclick',function(){
-	    infowindow.setMarker = null;
-	  });
-    var streetViewService = new google.maps.StreetViewService();
-    var radius = 8;
-    // In case the status is OK, which means the pano was found, compute the
-    // position of the streetview image, then calculate the heading, then get a
-    // panorama from that and set the options
-    function getStreetView(data, status) {
-      if (status == google.maps.StreetViewStatus.OK) {
-        var nearStreetViewLocation = data.location.latLng;
-        var heading = google.maps.geometry.spherical.computeHeading(
-          nearStreetViewLocation, marker.position);
-          infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
-          var panoramaOptions = {
-            position: nearStreetViewLocation,
-            pov: {
-              heading: heading,
-              pitch: 20
-            }
-          };
-        var panorama = new google.maps.StreetViewPanorama(
-          document.getElementById('pano'), panoramaOptions);
-      } else {
-        infowindow.setContent('<div>' + marker.title + '</div>' +
-          '<div>No Street View Found</div>');
+  }, 1400);
+  // Check to make sure the infowindow is not already opened on this marker.
+  if (infowindow.marker != marker) {
+    infowindow.marker = marker;
+    infowindow.setContent('');
+    $.ajax({
+      type: 'GET',
+      url: 'https://developers.zomato.com/api/v2.1/restaurant?res_id='+ marker.id,
+      beforeSend: function(xhr){xhr.setRequestHeader('user-key', '64c768e904a84d43d6df7fe90a1decee');},
+      success: function(response){
+        img_url = response["featured_image"];
+        infowindow.setContent('<div>' + marker.title + '</div><img src="'+img_url+'" style="width:280px;height:200px;"></div>');
+        
+      },
+      error: function(){
+        infowindow.setContent('<div>' + marker.title + '</div><div>No Image Found</div>');
+        //infowindow.open(map, marker); 
       }
-    }
-    // Use streetview service to get the closest streetview image within
-    // 50 meters of the markers position
-    streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+    });
+    // Make sure the marker property is cleared if the infowindow is closed.
+    infowindow.addListener('closeclick',function(){
+      infowindow.setMarker = null;
+    });
     infowindow.open(map, marker);
-	}
+  }
 }
 
 function makeMarkerIcon(markerColor) {
@@ -102,48 +86,48 @@ function makeMarkerIcon(markerColor) {
 }
 
 function setMarkersWithingBounds(bounds){
-	// set markers visible which are withing bounds
-	markers.forEach(function(marker){
-		if(bounds.contains(marker.position)){
-			marker.setMap(map);
-		}else{
-			marker.setMap(null);
-		}
-	});
+  // set markers visible which are withing bounds
+  markers.forEach(function(marker){
+    if(bounds.contains(marker.position)){
+      marker.setVisible(true);
+    }else{
+      marker.setVisible(false);
+    }
+  });
 }
 
 function zoomToArea(location){
-	// zoom to specific location
-	map.setCenter(location);
-	map.setZoom(16);
-	var bounds = map.getBounds();
-	setMarkersWithingBounds(bounds);
+  // zoom to specific location
+  map.setCenter(location);
+  map.setZoom(16);
+  var bounds = map.getBounds();
+  setMarkersWithingBounds(bounds);
 }
 
 function zoomToDefault(){
-	// zoom to default location
-	map.setCenter({lat: 28.459269, lng: 77.072419});
-	map.setZoom(13);
-	var bounds = map.getBounds();
-	setMarkersWithingBounds(bounds);
+  // zoom to default location
+  map.setCenter({lat: 28.459269, lng: 77.072419});
+  map.setZoom(13);
+  var bounds = map.getBounds();
+  setMarkersWithingBounds(bounds);
 }
 
 function animateMarker(rid){
-	//marker.setAnimation(google.maps.Animation.BOUNCE);
-	markers.forEach(function(marker){
-		if(marker.id == rid){
-			marker.setAnimation(google.maps.Animation.BOUNCE);
-		}
-	});
+  //marker.setAnimation(google.maps.Animation.BOUNCE);
+  markers.forEach(function(marker){
+    if(marker.id == rid){
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+  });
 }
 
 function stopAnimateMarker(rid){
-	//marker.setAnimation(google.maps.Animation.BOUNCE);
-	markers.forEach(function(marker){
-		if(marker.id == rid){
-			marker.setAnimation(null);
-		}
-	});
+  //marker.setAnimation(google.maps.Animation.BOUNCE);
+  markers.forEach(function(marker){
+    if(marker.id == rid){
+      marker.setAnimation(null);
+    }
+  });
 }
 
 function googleError(){
--- assets/js/view.js
+++ assets/js/view.js
@@ -1,86 +1,86 @@
 var RModel = function(title, rid){
-	this.title = title;
-	this.rid = rid;
+  this.title = title;
+  this.rid = rid;
 };
 
 // restaurants
 var restaurants = [];
 locations.forEach(function(loc){
-	restaurants.push(new RModel(loc.title, loc.rid));
+  restaurants.push(new RModel(loc.title, loc.rid));
 });
 
 var LocationModel = function(rests, located, locations){
-	this.rests = ko.observableArray(rests);
-	this.located = located;
-	this.locations = locations;
+  this.rests = ko.observableArray(rests);
+  this.located = located;
+  this.locations = locations;
 };
 
 // location specific collection
 var locations_ko = [
-	new LocationModel(restaurants.slice(0,5), 'Sector 29', {lat: 28.466945, lng: 77.06652}),
- 	new LocationModel(restaurants.slice(5,8), 'Galleria Market', {lat: 28.467252, lng: 77.08202}),
+  new LocationModel(restaurants.slice(0,5), 'Sector 29', {lat: 28.466945, lng: 77.06652}),
+  new LocationModel(restaurants.slice(5,8), 'Galleria Market', {lat: 28.467252, lng: 77.08202}),
   new LocationModel(restaurants.slice(8,10), 'Unitech CyberPark', {lat: 28.442781, lng: 77.057076}),
   new LocationModel(restaurants.slice(10,13), 'DLF Cyber Hub', {lat: 28.495391, lng: 77.088467})
 ];
 
 var viewModel = function(){
-	var self = this;
+  var self = this;
 
-	this.allLocations = ko.observableArray(restaurants);
-	this.availableLocations = ko.observableArray(locations_ko);
-	this.selectedLocation = ko.observable();
-	this.selectedLocation.subscribe(function(newval){
-		if(newval){
-			self.allLocations(null);
-			zoomToArea(newval.locations);
-		}else{
-			self.allLocations(restaurants);
-			zoomToDefault();
-		}
-	});
-	/*this.zoomToArea = function(){
-		debugger;
-	};*/
-	this.infoAvailable = ko.observable({
-		"cuisines": '',
-		"url": '',
-		"average_cost_for_two": '',
-		"error": ''
-	});
-	this.selectedRestaurant = function(){
-		var innerself = this;
-		// animate
-		animateMarker(this.rid);
-		window.setTimeout(function(){
-			stopAnimateMarker(innerself.rid);
-		}, 750);
+  this.allLocations = ko.observableArray(restaurants);
+  this.availableLocations = ko.observableArray(locations_ko);
+  this.selectedLocation = ko.observable();
+  this.selectedLocation.subscribe(function(newval){
+    if(newval){
+      self.allLocations(null);
+      zoomToArea(newval.locations);
+    }else{
+      self.allLocations(restaurants);
+      zoomToDefault();
+    }
+  });
+  /*this.zoomToArea = function(){
+    debugger;
+  };*/
+  this.infoAvailable = ko.observable({
+    "cuisines": '',
+    "url": '',
+    "average_cost_for_two": '',
+    "error": ''
+  });
+  this.selectedRestaurant = function(){
+    var innerself = this;
+    // animate
+    animateMarker(this.rid);
+    window.setTimeout(function(){
+      stopAnimateMarker(innerself.rid);
+    }, 1400);
 
-		var rid=this.rid;
-		var info = {};
-		//var info = getInfo(this.rid);
-		$.ajax({
-			type: 'GET',
-			url: 'https://developers.zomato.com/api/v2.1/restaurant?res_id='+rid,
-			beforeSend: function(xhr){xhr.setRequestHeader('user-key', '64c768e904a84d43d6df7fe90a1decee');},
-			success: function(response){
-				info["cuisines"] = response["cuisines"];
-				info["average_cost_for_two"] = response["currency"]+" "+response["average_cost_for_two"];
-				info["url"] = response["url"];
-				info["menu_url"] = response["menu_url"];
-				info["name"] = response["name"];
-				self.infoAvailable(info);
-			},
-			error: function(){
-				self.infoAvailable({'error': 'Error in Request'});
-			}
-		});
-	};
-	/*this.animateMarker = function(){
-		animateMarker(this.rid);
-	};
-	this.stopAnimateMarker = function(){
-		stopAnimateMarker(this.rid);
-	};*/
+    var rid=this.rid;
+    var info = {};
+    //var info = getInfo(this.rid);
+    $.ajax({
+      type: 'GET',
+      url: 'https://developers.zomato.com/api/v2.1/restaurant?res_id='+rid,
+      beforeSend: function(xhr){xhr.setRequestHeader('user-key', '64c768e904a84d43d6df7fe90a1decee');},
+      success: function(response){
+        info["cuisines"] = response["cuisines"];
+        info["average_cost_for_two"] = response["currency"]+" "+response["average_cost_for_two"];
+        info["url"] = response["url"];
+        info["menu_url"] = response["menu_url"];
+        info["name"] = response["name"];
+        self.infoAvailable(info);
+      },
+      error: function(){
+        self.infoAvailable({'error': 'Error in Request'});
+      }
+    });
+  };
+  /*this.animateMarker = function(){
+    animateMarker(this.rid);
+  };
+  this.stopAnimateMarker = function(){
+    stopAnimateMarker(this.rid);
+  };*/
 
 };
 
